119f2318245ed5ab4868d10a9e2c2937f588b32f
Neil Ashton <neil.m.ashton@gmail.com>
2020-08-01 15:22:13 -0400

    Augment sv.cmd to generate tabular output

--- src/sv/cmd.clj
+++ src/sv/cmd.clj
@@ -2,24 +2,27 @@
   (:require [clojure.tools.cli :refer [parse-opts]])
   (:require [clojure.string :as string])
   (:require [clojure.java.io :as io])
-  (:require [clojure.pprint :refer [pprint]])
+  (:require [clojure.pprint :refer [print-table]])
+  (:require [sv.model :as model])
   (:require [sv.parse :as parse]))
 
 (def opts
-  [["-s" "--sort-by" "Sort by (gender, date-asc, date-desc)"
-    :default "gender"
+  [["-s" "--sort-by SORT-TYPE" "Sort by (gender, date-asc, date-desc)"
+    :default :gender
     :parse-fn keyword
-    :validate #{"gender"
-                "date-asc"
-                "date-desc"}]
-   ["-i" "--ignore-errors" "Ignore errors"
-    :default false]
+    :validate [#{:gender :date-asc :date-desc}
+               "Must be one of gender, date-asc, or date-desc"]]
+   ["-i" "--ignore-errors" "Ignore errors"]
    ["-h" "--help" "Display this help message"]])
 
 (def separators
   {:csv ","
    :psv "[|]"
    :ssv " "})
+(def sorters
+  {:gender model/sort-by-gender-and-last-name
+   :date-asc model/sort-by-date-asc
+   :date-desc model/sort-by-date-desc})
 
 (defn help-message
   [options-summary]
@@ -58,18 +61,18 @@
 
 (defn validate-args
   [args]
-  (let [{:keys [options errors summary]} (parse-opts args opts)]
+  (let [{:keys [options arguments errors summary]} (parse-opts args opts)]
     (cond
       (:help options) {:exit-message (help-message summary)
                        :ok? true}
       errors {:exit-message (error-msg errors)}
       (not (seq args))
       {:exit-message (error-msg ["Missing filename arguments."])}
-      (seq (filenames->errors args))
+      (seq (filenames->errors arguments))
       {:exit-message (error-msg (map #(str "Bad filename or file not found: " %)
-                                     (filenames->errors args)))}
+                                     (filenames->errors arguments)))}
       :else {:parse? true
-             :filenames args
+             :filenames arguments
              :options options})))
 
 (defn exit
@@ -77,10 +80,22 @@
   (println msg)
   (System/exit status))
 
+(defn handle-filenames
+  [filenames options]
+  (let [[records errors] (parse/combine-parse-results (map parse-filename filenames))
+        sorter (get sorters (:sort-by options))]
+    (if (and (seq errors)
+             (not (:ignore-errors options)))
+      (exit 1 (error-msg ["Some files had errors."])) ; TODO: say more!!
+      (exit 0 (with-out-str
+                (print-table (->> records
+                                  sorter
+                                  (map model/record->display))))))))
+
 (defn -main [& args]
-  (let [{:keys [exit-message ok? parse? filenames]} (validate-args args)]
+  (let [{:keys [exit-message ok? parse? filenames options]} (validate-args args)]
     (if exit-message
       (exit (if ok? 0 1) exit-message)
       (if parse?
-        (exit 0 (pprint (parse/combine-parse-results (map parse-filename filenames))))
+        (handle-filenames filenames options)
         (exit 0 nil)))))
