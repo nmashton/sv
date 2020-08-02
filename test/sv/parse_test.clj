c8fee61b2b9525bfdf5d024eb0fe1744a821c1f8
Neil Ashton <neil.m.ashton@gmail.com>
2020-08-02 07:32:05 -0400

    Add example test for prep-for-cmd

--- test/sv/parse_test.clj
+++ test/sv/parse_test.clj
@@ -1,7 +1,19 @@
 (ns sv.parse-test
   (:require [clojure.test :as t])
-  (:require [sv.parse])
+  (:require [sv.parse :as parse])
   (:require [sv.testutil :refer [valid-quickchecked?]]))
 
 (t/deftest parse-lines
   (t/is (valid-quickchecked? 'sv.parse/parse-lines)))
+
+(def example-results-errors
+  [[{:sv.parse/data :valid-data-a}
+    {:sv.parse/data :valid-data-b}]
+   [{:sv.parse/line-number 1}
+    {:sv.parse/line-number 2}
+    {:sv.parse/line-number 3}]])
+
+(t/deftest prep-for-cmd
+  (t/is (= (parse/prep-for-cmd example-results-errors "test-data")
+           ['(:valid-data-a :valid-data-b)
+            ["test-data" '(1 2 3)]])))
