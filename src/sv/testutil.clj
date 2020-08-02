0ac07c11a6c1585e6b25f57c7532d747885eb2c3
Neil Ashton <neil.m.ashton@gmail.com>
2020-08-02 07:29:05 -0400

    Make valid-quickchecked? more informative

--- src/sv/testutil.clj
+++ src/sv/testutil.clj
@@ -1,10 +1,14 @@
 (ns sv.testutil
-  (:require [clojure.spec.test.alpha :as st]))
+  (:require [clojure.spec.test.alpha :as st])
+  (:require [clojure.pprint :as pp]))
 
 (defn valid-quickchecked?
   "Helper to wrap quickcheck calls in unit tests."
   [sym]
-  (-> (st/check sym)
-      first
-      :clojure.spec.test.check/ret
-      :pass?))
+  (let [result (-> (st/check sym)
+                   first
+                   :clojure.spec.test.check/ret)]
+    (if (:pass? result)
+      true
+      (do (pp/pprint result)
+          false))))
