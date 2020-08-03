1c3457bb420f955ea5bf606674e4e016527b2e9d
Neil Ashton <neil.m.ashton@gmail.com>
2020-08-02 21:52:48 -0400

    Add more sv.file tests

--- src/sv/file.clj
+++ src/sv/file.clj
@@ -4,9 +4,9 @@
             [sv.parse :as parse]))
 
 (def separators
-  {:csv ","
-   :psv "[|]"
-   :ssv " "})
+  {::csv ","
+   ::psv "\\n"
+   ::ssv " "})
 
 (def sorters
   {:gender model/sort-by-gender-and-last-name
@@ -17,17 +17,17 @@
 (defn filename->fmt
   [filename]
   (cond
-    (re-matches #"\S+\.csv" filename) :csv
-    (re-matches #"\S+\.psv" filename) :psv
-    (re-matches #"\S+\.ssv" filename) :ssv
+    (re-matches #"\S+\.csv" filename) ::csv
+    (re-matches #"\S+\.psv" filename) ::psv
+    (re-matches #"\S+\.ssv" filename) ::ssv
     :else nil))
 
 (defn guess-format
   [raw-line]
   (cond
-    (re-matches #"^(\S+,){4}\S+$" raw-line) :csv
-    (re-matches #"^(\S+\|){4}\S+$" raw-line) :psv
-    (re-matches #"^(\S+ ){4}\S+$" raw-line) :ssv
+    (re-matches #"^(\S+,){4}\S+$" raw-line) ::csv
+    (re-matches #"^(\S+\|){4}\S+$" raw-line) ::psv
+    (re-matches #"^(\S+ ){4}\S+$" raw-line) ::ssv
     :else nil))
 
 (defn filenames->errors
--- test/sv/file_test.clj
+++ test/sv/file_test.clj
@@ -3,7 +3,28 @@
             [sv.file :as file]))
 
 (deftest filename->fmt
-  (is (= :csv (file/filename->fmt "foo.csv")))
-  (is (= :psv (file/filename->fmt "bar.psv")))
-  (is (= :ssv (file/filename->fmt "baz.ssv")))
+  (is (= :sv.file/csv (file/filename->fmt "foo.csv")))
+  (is (= :sv.file/psv (file/filename->fmt "bar.psv")))
+  (is (= :sv.file/ssv (file/filename->fmt "baz.ssv")))
   (is (nil? (file/filename->fmt "qux.xml"))))
+
+(deftest guess-format
+  (is (= :sv.file/csv (file/guess-format "foo,bar,baz,qux,norf")))
+  (is (= :sv.file/psv (file/guess-format "foo|bar|baz|qux|norf")))
+  (is (= :sv.file/ssv (file/guess-format "foo bar baz qux norf")))
+  (is (nil? (file/guess-format "not,a,valid,csv")))
+  (is (nil? (file/guess-format "not|a|valid|psv")))
+  (is (nil? (file/guess-format "not a valid ssv")))
+  (is (nil? (file/guess-format "just ..... some gibberish"))))
+
+(deftest filenames->errors
+  (is (file/filenames->errors ["foo.bar"]))
+  (is (let [temp-file (java.io.File/createTempFile "file" ".bad-extension")]
+        (try
+          (file/filenames->errors [(.getAbsolutePath temp-file)])
+          (finally (.delete temp-file)
+                   nil))))
+  (is (empty? (let [temp-file (java.io.File/createTempFile "file" ".csv")]
+                (try
+                  (doall (file/filenames->errors [(.getAbsolutePath temp-file)]))
+                  (finally (.delete temp-file)))))))
